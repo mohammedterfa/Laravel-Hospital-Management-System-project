8dc7208970f1cd52407f18f36a01cc656f5f04b6
mohammedterfa <mohammedterfa@gmail.com>
2022-03-23 21:15:54 +0200

    edit doctor data in admin panel was now working

--- resources/views/admin/showdoctor.blade.php
+++ resources/views/admin/showdoctor.blade.php
@@ -53,7 +53,7 @@
                         <td>{{ $doctor->speciality }}</td>
                         <td>{{ $doctor->room }}</td>
                         <td><img height="100" width="100" src="doctorimage/{{ $doctor->image }}" alt=""></td>
-                        <td><a  class="btn btn-primary" href="">Update</a></td>
+                        <td><a  class="btn btn-primary" href="{{ url('updatedoctor', $doctor->id) }}">Update</a></td>
                         <td><a class="btn btn-danger" onclick="return confirm('are you sure to delete this doctor ?')" href="{{ url('deletedoctor', $doctor->id) }}">Delete</a></td>
                     </tr>
 
--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::get('/showdoctor',[AdminController::class, 'showdoctor']);
 
 Route::get('/deletedoctor/{id}',[AdminController::class, 'deletedoctor']);
 
+Route::get('/updatedoctor/{id}',[AdminController::class, 'updatedoctor']);
+
+Route::post('/editdoctor/{id}',[AdminController::class, 'editdoctor']);
+
